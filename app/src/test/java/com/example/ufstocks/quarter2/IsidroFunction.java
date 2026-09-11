bdfd124313c4196befe182c5807f4633376f8c3f
keziah <mskeishaisidro@gmail.com>
2026-09-12 03:36:11 +0800

    Isidro's Profile Test

---
+++ app/src/test/java/com/example/ufstocks/quarter2/IsidroFunction.java
@@ -0,0 +1,20 @@
+package com.example.ufstocks.quarter2;
+
+import org.junit.Test;
+
+public class IsidroFunction {
+    @Test
+    public void printMyProfile() {
+        // --1. THE INPUT (Storing your personal details in variables)
+        String myName = "Keziah";
+        String petName = "Mochi";
+        String favFood = "Takoyaki";
+        int myAge = 16;
+
+        // --- 2. THE OUTPUT (Printing to the console)
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
